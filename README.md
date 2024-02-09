693a14110b9be5926549e244048d3fbb47833d7f
hyang0 <boo_126@126.com>
2024-02-09 08:50:00 +0800

    add script and wubi dict
    Signed-off-by: hyang0 <boo_126@126.com>

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # wubi
+
 显示汉字的拼音和五笔编码
+
+用法：
+
+```bash
+bash$ python wubi.py -h
+usage: wubi.py [-h] [--wubi_file WUBI_FILE] file_path
+
+汉字拼音和五笔编码查询工具
+
+positional arguments:
+  file_path             包含汉字的文件路径
+
+options:
+  -h, --help            show this help message and exit
+  --wubi_file WUBI_FILE
+                        五笔编码文件路径，默认为 wubi86.dict
+
+bash$ python wubi.py 百家姓.txt | head
+汉字: 赵钱孙李
+拼音: zhao qian sun li
+五笔: fhqi qgt biy sbf
+
+汉字: 周吴郑王
+拼音: zhou wu zheng wang
+五笔: mfkd kgdu udbh gggg
+
+```
---
+++ wubi.py
@@ -0,0 +1,65 @@
+import argparse
+from pypinyin import pinyin, Style
+import platform
+import os
+import sys
+
+
+def change_default_encoding():
+    '''判断是否在 windows git-bash 下运行，是则使用 utf-8 编码'''
+    if platform.system() == 'Windows':
+        terminal = os.environ.get('TERM')
+        if terminal and 'xterm' in terminal:
+            sys.stdout.reconfigure(encoding='utf-8')
+
+
+def is_empty_line(line):
+    return line.isspace()
+
+def load_wubi_dict(file_path):
+    wubi_dict = {}
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            char, wubi_code = line.strip().split(' ')
+            wubi_dict[char] = wubi_code
+    return wubi_dict
+
+def get_pinyin(character):
+    pinyin_result = pinyin(character, style=Style.NORMAL)
+    pinyin_str = ''.join([item[0] for item in pinyin_result])
+    return pinyin_str
+
+def get_wubi(character, wubi_dict):
+    wubi_str = wubi_dict.get(character)
+    if not wubi_str:
+        return character
+    return wubi_str
+
+if __name__ == "__main__":
+    change_default_encoding()
+    parser = argparse.ArgumentParser(description="汉字拼音和五笔编码查询工具")
+    parser.add_argument("file_path", help="包含汉字的文件路径")
+    parser.add_argument("--wubi_file", default=r"wubi86.dict", help="五笔编码文件路径，默认为 wubi86.dict")
+    args = parser.parse_args()
+
+    wubi_dict = load_wubi_dict(args.wubi_file)
+
+    with open(args.file_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            characters = line.strip()
+            if is_empty_line(line):
+                continue
+            py_list = []
+            wb_list = []
+
+            for char in characters:
+                pinyin_result = get_pinyin(char)
+                wubi_result = get_wubi(char, wubi_dict)
+
+                py_list.append(pinyin_result)
+                wb_list.append(wubi_result)
+
+            print("汉字:", characters)
+            print("拼音:", ' '.join(py_list))
+            print("五笔:", ' '.join(wb_list))
+            print()
